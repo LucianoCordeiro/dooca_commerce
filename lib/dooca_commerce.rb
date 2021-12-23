e16fb329a7e70aa5c51ba5bfa7842164364785cf
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2021-12-23 20:03:31 -0300

    add more requests and create response handler class

--- lib/dooca_commerce.rb
+++ lib/dooca_commerce.rb
@@ -1,29 +1,48 @@
 require 'faraday'
+require 'response_handler'
 
 class DoocaCommerce
 
   def initialize(token:)
     @token = token
+    @handler = ResponseHandler
   end
 
-  def fetch_products(query: nil)
+  attr_reader :token, :handler
+
+  def fetch_products(query = {})
     response = Faraday.get("#{URL}/products", query, header)
 
-    parse_response(response)
+    handler.new(response, list: true).parse!
+  end
 
-    self
+  def fetch_product(query = {})
+    response = Faraday.get("#{URL}/products", query, header)
+
+    handler.new(response).parse!
+  end
+
+  def fetch_variations(query = {})
+    response = Faraday.get("#{URL}/variations", query, header)
+
+    handler.new(response, list: true).parse!
+  end
+
+  def fetch_variation(query = {})
+    response = Faraday.get("#{URL}/variations", query, header)
+
+    handler.new(response).parse!
+  end
+
+  def update_stock_and_price(product_id:, body:)
+    response = Faraday.put("#{URL}/products/#{product_id}", body.to_json, header)
+
+    handler.new(response).parse!
   end
 
   private
 
   URL = "https://api.dooca.store"
-
-  attr_reader :token, :body, :status
-
-  def parse_response(response)
-    @body = JSON.parse(response.body).deep_symbolize_keys
-    @status = response.status
-  end
 
   def header
     {"Authorization": "Bearer #{token}"}
---
+++ lib/response_handler.rb
@@ -0,0 +1,39 @@
+require 'faraday'
+
+class ResponseHandler
+
+  def initialize(response, list: false)
+    @response = response
+    @list = list
+  end
+
+  attr_reader :response, :list
+
+  def parse!
+    success? ? handle_success : handle_error
+  end
+
+  private
+
+  def handle_success
+    @payload = payload.data.first if payload.data.present? && !list
+
+    payload.success = true
+
+    payload
+  end
+
+  def handle_error
+    payload.success = false
+
+    payload
+  end
+
+  def payload
+    @payload ||= JSON.parse(response.body, object_class: OpenStruct)
+  end
+
+  def success?
+    response.status == 200
+  end
+end
